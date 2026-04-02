a67bf827934d25c1fe475536fb7fe72589a40e1a
Sommanek Akshat <akshatsommanek99@gmail.com>
2026-04-03 01:06:50 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+HYPHEN_E_DOT="-e ."
+def get_requirements(file_path:str)->list[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name="IPL Project",
+    version='0.0.1',
+    author="Akshat",
+    author_email='akshatsommanek@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
